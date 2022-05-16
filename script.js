b99620d6fe8044fafea5defdab18ca34594aa98d
Sarah <sa4913@nyu.edu>
2022-05-16 10:46:33 +0800

    fixing a bug with the pop-up

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ window.addEventListener("scroll", reveal);
 
 function closeIframe(){
   iframeWrapper.style.display = "none";
+  actualIframe.src = "startingBlank";
 }
 
 function openIframe(page){
